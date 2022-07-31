3ccaf4ac135118c3e197330c609d235d5b0cf5be
Daky <daky>
2022-07-31 15:08:42 +0200

    Initial commit: base project

---
+++ NoIntro/Main.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[assembly:MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly:MelonInfo(typeof(NoIntro.NoIntroMod), "NoIntro", "1.0.0", "daky", "https://github.com/dakyneko/DakyModsCVR")]
+
+
+namespace NoIntro
+{
+    public class NoIntroMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg($"Hello mod!");
+        }
+    }
+}
