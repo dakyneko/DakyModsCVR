05b123b4b6bb268dc1a99fd13b68576f6f8c2fc4
Daky <daky>
2022-08-13 16:14:18 +0200

    Star: first version with alpha bg + locker + near+far settings

---
+++ CameraStar/CameraStar.cs
@@ -0,0 +1,156 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+using ABI_RC.Systems.Camera;
+using ABI.CCK.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Daky;
+
+using OpCodes = System.Reflection.Emit.OpCodes;
+using RefFlags = System.Reflection.BindingFlags;
+
+[assembly:MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly:MelonInfo(typeof(CameraStar.CameraStarMod), "CameraStar", "1.0.0", "daky", "https://github.com/dakyneko/DakyModsCVR")]
+
+namespace CameraStar
+{
+    using static Dakytils;
+    public class CameraStarMod : MelonMod
+    {
+        private static MelonLogger.Instance logger;
+        public static bool disableCameraFadeout = false;
+
+        public override void OnApplicationStart()
+        {
+            logger = LoggerInstance;
+
+            HarmonyInstance.Patch(
+                SymbolExtensions.GetMethodInfo(() => default(PortableCamera).Start()),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(CameraStarMod), nameof(OnPortableCameraStart))));
+            HarmonyInstance.Patch(
+                SymbolExtensions.GetMethodInfo(() => default(PortableCamera).Update()),
+                transpiler: new HarmonyMethod(AccessTools.Method(typeof(CameraStarMod), nameof(TranspilePortableCameraUpdate))));
+        }
+        // TODO: there are too many built-in mods, we should have options to hide them or group them (like vrc)
+        // TODO: add option to disable PortableCamera fading transparent when 'inactive' so annoying
+
+        private static IEnumerable<CodeInstruction> TranspilePortableCameraUpdate(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var idx = codes.FindIndex(x => x.StoresField(typeof(PortableCamera).GetField("_fadeOutDistance", RefFlags.NonPublic | RefFlags.Instance)));
+            idx -= 7; // jump to first ldarg.0
+            switch (true) {
+                case true:
+                    if (idx < 0) goto case false;
+                    if (codes.ElementAt(idx).opcode != OpCodes.Ldarg_0) goto case false;
+                    if (codes.ElementAt(idx + 1).opcode != OpCodes.Ldarg_0) goto case false;
+                    if (codes.ElementAt(idx + 121).opcode != OpCodes.Ret) goto case false; // check there is nothing else at the end
+                    codes.Insert(idx, new CodeInstruction(OpCodes.Ret));
+                    break;
+
+                case false:
+                    logger.Warning("TranspilePortableCameraUpdate failure");
+                    break;
+            }
+            return codes.AsEnumerable();
+        }
+
+        private static void OnPortableCameraStart(PortableCamera __instance)
+        {
+            __instance.RegisterMod(new AlphaTransparency());
+            __instance.RegisterMod(new CameraLocker());
+
+            __instance.@interface.AddAndGetHeader(null, typeof(CameraStarMod), "Camera*");
+            NewCameraSetting(__instance, "hideworld", "Hide world", typeof(CameraStarMod), false, v =>
+            {
+                if (v) __instance._camera.cullingMask &=
+                    (1 << LayerMask.NameToLayer("PlayerLocal")) |
+                    (1 << LayerMask.NameToLayer("PlayerNetwork"));
+                else __instance.RestoreLayerMask();
+            });
+
+            NewCameraSetting(__instance, "clipnear", "Near Clipping", typeof(CameraStarMod), 0f, minValue: 0f, maxValue: 1f,
+                onChange: v => __instance._camera.nearClipPlane = 0.01f + Mathf.Exp(5 * v) - 1);
+
+            NewCameraSetting(__instance, "clipfar", "Far Clipping", typeof(CameraStarMod), 1f, minValue: 0f, maxValue: 1f,
+                onChange: v => __instance._camera.farClipPlane = 0.01f + Mathf.Exp(7 * v) - 1);
+        }
+    }
+
+    public class CameraLocker : ICameraVisualMod
+    {
+        private PortableCamera _portable = null;
+        private Sprite _icon;
+        private Transform _cvrCamera20;
+        private Transform _modsButtonParent;
+        private Transform _sideButtonParent;
+
+        public CameraLocker() => _icon = SpriteFromAssembly(this.GetType().Namespace, "locker.png");
+        public string GetModName(string language) => "Lock";
+        public Sprite GetModImage() => _icon;
+        public int GetSortingOrder() => 42;
+        public bool ActiveIsOrange() => false;
+        public bool DefaultIsOn() => false;
+        public void Setup(PortableCamera portableCam, Camera camera)
+        {
+            _portable = portableCam;
+            _cvrCamera20 = portableCam.transform.parent;
+            var canvas = _cvrCamera20.Find("Content/Camera Canvas");
+            _modsButtonParent = canvas.Find("VisualMods/Scroll View/Viewport/Content");
+            _sideButtonParent = canvas.Find("Background");
+        }
+
+        private void SetState(bool active)
+        {
+            var camPickup = _cvrCamera20.GetComponent<CVRPickupObject>();
+            if (camPickup != null) camPickup.enabled = !active;
+            foreach (Transform t in _modsButtonParent)
+            {
+                if (t.GetComponent<VisualMod>()?.mod == this) continue;
+                t.gameObject.SetActive(!active);
+            }
+            foreach (Transform t in _sideButtonParent)
+            {
+                if (!t.name.StartsWith("Button")) continue;
+                t.gameObject.SetActive(!active);
+            }
+            var mod = _portable.loadedMods[this];
+            mod?.tooltipText?.SetText(active ? "Unlock" : "Lock");
+        }
+
+        public void Enable() => SetState(true);
+        public void Disable() => SetState(false);
+    }
+
+    public class AlphaTransparency : ICameraVisualMod
+    {
+        private PortableCamera _portable = null;
+        private Camera _camera = null;
+        private Sprite _icon;
+
+        public AlphaTransparency() => _icon = SpriteFromAssembly(this.GetType().Namespace, "opacity-512.png");
+        public string GetModName(string language) => "Alpha Transparency";
+        public Sprite GetModImage() => _icon;
+        public int GetSortingOrder() => 40;
+        public bool ActiveIsOrange() => false;
+        public bool DefaultIsOn() => false;
+        public void Setup(PortableCamera portableCam, Camera camera)
+        {
+            _portable = portableCam;
+            _camera = camera;
+        }
+
+        public void Enable()
+        {
+            _camera.cullingMask &= 5376; // same as in Camera Greensceen mod
+            _camera.clearFlags = CameraClearFlags.Color;
+            _camera.backgroundColor = Color.clear;
+        }
+        public void Disable()
+        {
+            _portable.RestoreLayerMask();
+        }
+    }
+}
---
+++ Common/Common.cs
@@ -0,0 +1,66 @@
+using ABI_RC.Systems.Camera;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Daky
+{
+    public static class Dakytils
+    {
+        public static Sprite SpriteFromAssembly(string namespace_, string filename, int width = 512, int height = 512)
+        {
+            using var stream = System.Reflection.Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream(namespace_ + "." + filename);
+            if (stream == null || stream.Length == 0) return null;
+            using var memStream = new MemoryStream((int)stream.Length);
+            stream.CopyTo(memStream);
+            var texture = new Texture2D(512, 512, TextureFormat.RGBA32, false);
+            texture.LoadImage(memStream.ToArray());
+            return Sprite.Create(texture, new Rect(0, 0, width, height), new Vector2(0, 0));
+        }
+
+        // boiler plate woooh
+        private static PortableCameraSetting NewCameraSetting(PortableCamera pcam, string name, string display,
+            PortableCameraSettingType type, Type origin, object initialValue)
+        {
+            var s = pcam.@interface.AddAndGetSetting(type);
+            s.SettingName = name;
+            s.DisplayName = display;
+            s.OriginType = origin;
+            s.DefaultValue = initialValue;
+            return s;
+        }
+
+        public static PortableCameraSetting NewCameraSetting(PortableCamera pcam, string name, string display,
+            Type origin, bool initialValue, Action<bool> onChange)
+        {
+            var s = NewCameraSetting(pcam, name, display, PortableCameraSettingType.Bool, origin, initialValue);
+            s.BoolChanged = onChange;
+            s.Load();
+            return s;
+        }
+
+        public static PortableCameraSetting NewCameraSetting(PortableCamera pcam, string name, string display,
+            Type origin, int initialValue, Action<int> onChange, int minValue = 0, int maxValue = 100)
+        {
+            var s = NewCameraSetting(pcam, name, display, PortableCameraSettingType.Int, origin, initialValue);
+            s.IntChanged = onChange;
+            s.MinValue = minValue;
+            s.MaxValue = maxValue;
+            s.Load();
+            return s;
+        }
+
+        public static PortableCameraSetting NewCameraSetting(PortableCamera pcam, string name, string display,
+            Type origin, float initialValue, Action<float> onChange, float minValue = 0f, float maxValue = 1f)
+        {
+            var s = NewCameraSetting(pcam, name, display, PortableCameraSettingType.Float, origin, initialValue);
+            s.FloatChanged = onChange;
+            s.MinValue = minValue;
+            s.MaxValue = maxValue;
+            s.Load();
+            // TODO: CVR has bug when values are 0 it consider null maybe and show _Value instead :(
+            return s;
+        }
+    }
+}
