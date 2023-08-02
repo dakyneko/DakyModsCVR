1449554b8a72fc03621c2a19a8f4ea7b4df29a59
Daky <daky>
2023-08-02 15:12:28 +0200

    prepare build for new cvr 2023r171 (now .net 4.8.1)

--- copy_and_nstrip_dll.bat
+++ copy_and_nstrip_dll.bat
@@ -2,9 +2,12 @@
 setlocal enableDelayedExpansion
 
 echo Copy all dll from the game so we can work and modify them
-xcopy /f /y "%CVRPATH%\MelonLoader\0Harmony.dll" "%0\..\ManagedLibs"
-xcopy /f /y "%CVRPATH%\MelonLoader\MelonLoader.dll" "%0\..\ManagedLibs"
+xcopy /f /y "%CVRPATH%\MelonLoader\net35\0Harmony.dll" "%0\..\ManagedLibs"
+xcopy /f /y "%CVRPATH%\MelonLoader\net35\MelonLoader.dll" "%0\..\ManagedLibs"
 xcopy /f /s /y "%CVRPATH%\ChilloutVR_Data\Managed" "%0\..\ManagedLibs"
+
+Rem this breaks vstudio build system it seems
+del ""%0\..\ManagedLibs\netstandard.dll"
 
 echo Nstrip convert all private/protected stuff to public, yay
 for %%x in (Assembly-CSharp.dll Assembly-CSharp-firstpass.dll UnityEngine.CoreModule.dll Cohtml.Runtime.dll) do (
