86dae5d766772d7ffa5a2964486b7294f804ac3d
kafeijao <kafeijao@gmail.com>
2024-07-27 15:02:28 +0100

    [NoIntro] Fixed for 2024r176
    +thanks

--- NoIntro/NoIntro.cs
+++ NoIntro/NoIntro.cs
@@ -1,27 +1,12 @@
-﻿using HarmonyLib;
-using MelonLoader;
-using SkipIntro = ABI_RC.Core.Savior.SkipIntro;
-using RefFlags = System.Reflection.BindingFlags;
+﻿using MelonLoader;
 
 [assembly:MelonGame("Alpha Blend Interactive", "ChilloutVR")]
-[assembly:MelonInfo(typeof(NoIntro.NoIntroMod), "NoIntro", "1.0.1", "daky", "https://github.com/dakyneko/DakyModsCVR")]
+[assembly:MelonInfo(typeof(NoIntro.NoIntroMod), "NoIntro", "1.0.2", "daky", "https://github.com/dakyneko/DakyModsCVR")]
 
 namespace NoIntro
 {
     public class NoIntroMod : MelonMod
     {
-        public override void OnInitializeMelon()
-        {
-            // Thank you DragonPlayer for your help ;)
-            HarmonyInstance.Patch(
-                typeof(SkipIntro).GetMethod(nameof(SkipIntro.Start),  RefFlags.Instance | RefFlags.NonPublic),
-                new HarmonyMethod(AccessTools.Method(typeof(NoIntroMod), nameof(OnStart))));
-        }
-
-        private static bool OnStart(SkipIntro __instance)
-        {
-            __instance.Skip();
-            return false;
-        }
+        public override void OnInitializeMelon() => ABI_RC.Core.Savior.IntroManager.Skip(); // thanks kafe
     }
 }
